83f99771e6d6666e652c2dc50f6a85a6d2936f5b
Ritidube <ritidubey245@gmail.com>
2023-11-17 19:17:06 +0530

    new

--- tkin.py/category.py
+++ tkin.py/category.py
@@ -19,7 +19,43 @@ class categoryClass:
 
         btn_add=Button(self.root,text="ADD",font=("goudy old style",18),bg="#4caf50",fg="white",cursor="hand2").place(x=360,y=170,width=150,height=30)
         btn_delete=Button(self.root,text="Delete",font=("goudy old style",18),bg="red",fg="white",cursor="hand2").place(x=520,y=170,width=150,height=30)
-        
+
+
+#---------Category Frame--------
+        cat_frame=Frame(self.root,bd=3,relief=RIDGE)
+        cat_frame.place(x=700,y=100,width=380,height=100)
+
+        scrolly=Scrollbar(cat_frame,orient=VERTICAL)
+        scrollx=Scrollbar(cat_frame,orient=HORIZONTAL)
+
+
+        self.categoryTable=ttk.Treeview(cat_frame,columns=("cid","name"),yscrollcommand=scrolly.set,xscrollcommand=scrollx.set)
+        scrollx.pack(side=BOTTOM,fill=X)
+        scrolly.pack(side=RIGHT,fill=Y)
+        scrollx.config(command=self.categoryTable.xview)
+        scrolly.config(command=self.categoryTable.yview)
+        self.categoryTable.heading("cid",text="C ID")
+        self.categoryTable.heading("name",text="Name")
+       
+        self.categoryTable["show"]="headings"
+       
+        self.categoryTable.column("cid",width=90)
+        self.categoryTable.column("name",width=100)
+        self.categoryTable.pack(fill=BOTH,expand=1)  
+#-------------Images---------------
+        self.im1=Image.open("tkin.py/Images/cat1.jpg")
+        self.im1=self.im1.resize((500,250))
+        self.im1=ImageTk.PhotoImage(self.im1)
+
+        self.lbl_im1=Label(self.root,image=self.im1,bd=2,relief=RAISED)
+        self.lbl_im1.place(x=50,y=220)
+
+        self.im2=Image.open("tkin.py/Images/cat2.jpg")
+        self.im2=self.im2.resize((500,250))
+        self.im2=ImageTk.PhotoImage(self.im2)
+
+        self.lbl_im2=Label(self.root,image=self.im2,bd=2,relief=RAISED)
+        self.lbl_im2.place(x=580,y=220)
 
 
 if __name__=="__main__":
