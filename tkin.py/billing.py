8a0989ff02ccce442bb079178ffcb88fded236b7
Tanistha0904 <tanisthakeshri@gmail.com>
2023-11-18 20:59:55 +0530

    bill2

--- tkin.py/billing.py
+++ tkin.py/billing.py
@@ -9,9 +9,9 @@ class BillClass:
         self.root.config(bg="white")
         #headingtitle
         orignal_image=Image.open("Inventory-Management-System/tkin.py/Images/Logo.png")
-        resized_image=orignal_image.resize((150, 70))
+        resized_image=orignal_image.resize((80, 60))
         self.icon_title=ImageTk.PhotoImage(resized_image)
-        title=Label(self.root,text="Invntory Managemen System",image=self.icon_title,compound=LEFT,font=("times new roman",40,"bold"),bg="#010c48",fg="white",anchor="w",padx=20).place(x=0,y=0,relwidth=1,height=70)
+        title=Label(self.root,text="Inventory Managemen System",image=self.icon_title,compound=LEFT,font=("times new roman",40,"bold"),bg="#010c48",fg="white",anchor="w",padx=20).place(x=0,y=0,relwidth=1,height=70)
 
         #button__logout
         btn_logout=Button(self.root,text="Logout",font=("times new roman",15,"bold"),bg="yellow",cursor="hand2").place(x=1150,y=10,height=50,width=150)
@@ -30,6 +30,7 @@ class BillClass:
         ProductFrame2.place(x=2,y=42,width=398,height=90)
        
         lbl_search=Label(ProductFrame2,text="Search Product | By Name ",font=("times new roman",15,"bold"),bg="white",fg="green").place(x=2,y=5)
+        
         lbl_search=Label(ProductFrame2,text="Product Name",font=("times new roman",15,"bold"),bg="white").place(x=2,y=45)
         txt_search=Entry(ProductFrame2,textvariable=self.var_search,font=("times new roman",15),bg="lightyellow").place(x=128,y=47,width=150,height=22)
         btn_search=Button(ProductFrame2,text="Search",font=("goudy old style",15),bg="#2196f3",fg="white",cursor="hand2").place(x=285,y=45,width=100,height=25)
@@ -61,6 +62,18 @@ class BillClass:
         self.product_Table.pack(fill=BOTH,expand=1)
         #self.product_Table.bind("<ButtonRelease-1>",self.get_data)
         lbl_note=Label(ProductFrame1,text="Note: 'Enter 0 Quantity to remove product from the Cart'",font=("goundy old style",12),anchor='w',bg="white",fg="red").pack(side=BOTTOM,fill=X)
+        #======CustomerFrame===
+        self.var_name=StringVar()
+        self.var_contact=StringVar()
+        CustomerFrame=Frame(self.root,bd=4,relief=RIDGE,bg="white")
+        CustomerFrame.place(x=420,y=110,width=530,height=70)
+        cTitle=Label(CustomerFrame,text="Customer Details",font=("goudy old style",15),bg="lightgray").pack(side=TOP,fill=X)
+        lbl_name=Label(CustomerFrame,text="Name",font=("times new roman",15),bg="white").place(x=5,y=35)
+        txt_name=Entry(CustomerFrame,textvariable=self.var_name,font=("times new roman",13),bg="lightyellow").place(x=80,y=35,width=180)
+        lbl_contact=Label(CustomerFrame,text="Name",font=("times new roman",15),bg="white").place(x=5,y=35)
+        txt_contact=Entry(CustomerFrame,textvariable=self.var_name,font=("times new roman",13),bg="lightyellow").place(x=80,y=35,width=180)
+
+
 if __name__=="__main__":
     root=Tk()
     obj=BillClass(root)
